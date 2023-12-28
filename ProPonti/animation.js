65d5248dda349fe38ae1a7f76a789d7320786d1d
iDenisman <145935949+iDenisman@users.noreply.github.com>
2023-12-29 00:21:39 +0100

    ProPonti progress
    Webstránka s fungujucim slideshow. Fungujúci a správny header aj footer.

--- ProPonti/animation.js
+++ ProPonti/animation.js
@@ -15,14 +15,24 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  
+  // Ensure slideIndex is within valid range
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Remove "active" class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  // Display the current slide
+  slides[slideIndex - 1].style.display = "block";
+
+  // Add "active" class to the corresponding dot
+  dots[slideIndex - 1].className += " active";
 }
